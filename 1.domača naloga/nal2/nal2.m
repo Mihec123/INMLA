7f3c65264ac5052940f29a1f30e511618ff125e8
Mihec123 <mihavsec@gmail.com>
2017-12-17 18:26:46 +0100

    Popravek
    Dodani primeri pri nalogi 2, ter urejena naloga 3

--- 1.domača naloga/nal2/nal2.m
+++ 1.domača naloga/nal2/nal2.m
@@ -1,35 +1,48 @@
 %naloga2
 
-napaka = 1e-10;
+napaka = 1e-20;
 maxit = 200;
 
 load('DL1.mat');
 A = Problem.A;
 b = Problem.b;
-
+y1 = A\b;
 [x1,res1] = DLanczosPivot(A,b,zeros(length(b),1),napaka,maxit);
+norm(x1-y1,Inf)
 
 load('DL2.mat');
 A = Problem.A;
 b = Problem.b;
-
+y2 = A\b;
 [x2,res2] = DLanczosPivot(A,b,zeros(length(b),1),napaka,maxit);
+norm(x2-y2,Inf)
 
 load('DL3.mat');
 A = Problem.A;
 b = Problem.b;
-
+y3 = A\b;
 [x3,res3] = DLanczosPivot(A,b,zeros(length(b),1),napaka,maxit);
+norm(x3-y3,Inf)
 
 load('DL4.mat');
 A = Problem.A;
 b = Problem.b;
-
+y4 = A\b;
 [x4,res4] = DLanczosPivot(A,b,zeros(length(b),1),napaka,maxit);
+norm(x4-y4,Inf)
 
 %matrika kjer je potrebno pivotirati dvakrat zapored
-load('D5.mat');
+load('Test1.mat');
 A = Problem.A;
 b = Problem.b;
+y5 = A\b;
+[x5,res5] = DLanczosPivot(A,b,zeros(length(b),1),1e-20,4);
+norm(x5-y5,Inf)
 
-[x4,res4] = DLanczosPivot(A,b,zeros(length(b),1),1e-20,4);
+%matrika kjer ne pivotiramo
+load('Test2.mat');
+A = Problem.A;
+b = Problem.b;
+y6 = A\b;
+[x6,res6] = DLanczosPivot(A,b,zeros(length(b),1),1e-20,6);
+norm(x6-y6,Inf)
--- 1.domača naloga/nal3/nal3.m
+++ 1.domača naloga/nal3/nal3.m
@@ -1,72 +1,35 @@
-N = 1000;
-napaka = 1e-6;
+%naloga3
 
-%matrika c-63
-%matrika je simetricna in ni pozitivno definitna
-load('1224')
+ponovni = [1:1:30];
+N=500;
+napaka = 1e-6;
+alfa = 0.1;
+
+%matrika 1224
+[casi,ost,koraki,konvergenca] = test1224(napaka,N)
+pause;
+[casi,ost,koraki,konvergenca]=test1224LU(alfa,napaka,N)
+pause;
+load('1224.mat')
 A = Problem.A;
 b = Problem.b;
-x0 = zeros(length(A),1);
-%konvergenca pocasna
+[iteracije,ost,cas,konvergenca] = gmresPonovni(A,b,napaka,N,ponovni,alfa)
+pause;
 
-setup.type ='nofill';
-setup.droptol = 0.1;
-[L,U] = ilu(A,setup);
+%matrika 1311
+[casi,ost,koraki,konvergenca]=test1311(napaka,N)
+pause;
+load('1311.mat')
+A = Problem.A;
+b = A*ones(length(A),1);
+[iteracije,ost,cas,konvergenca] = gmresPonovni(A,b,napaka,N,ponovni)
+pause;
 
-% opcije.type = 'ict';
-% opcije.droptol = 0.005;
-% L1 = ichol(A,opcije);
-
-
-%1000 korakov gmres brez ponovnega zagona ostanek se vedno 0.01
-% ponovni= [5:5:50];
-% n = length(ponovni);
-% iteracije = zeros(n,1);
-% ost = zeros(n,1);
-% cas = zeros(n,1);
-% 
-% 
-% for i=1:n
-%     tic;
-%     [X1,FLAG1,RELRES1,ITER1] = gmres(A,b,[ponovni(i)],napaka,N,L,U);
-%     trajanje = toc;
-%     iteracije(i) = ITER1(1) * ITER1(2);
-%     ost(i) = RELRES1;
-%     cas(i) = trajanje;
-% end
-%[X2,FLAG2,RELRES2,ITER2] = minres(A,b,napaka,N);
-%matrika ni pozitivno definitna
-%[X3,FLAG3,RELRES3,ITER3] = pcg(A,b,napaka,N);
-%[X4,FLAG4,RELRES4,ITER4] = bicg(A,b,napaka,N);
-%[X5,FLAG5,RELRES5,ITER5] = qmr(A,b,napaka,N,L,U);
-%[X6,FLAG6,RELRES6,ITER6] = symmlq(A,b,napaka,N);
-%matrika ni pozitivno definitna
-%[X7,FLAG7,RELRES7,ITER7] = bicgstab(A,b,napaka,N);
-
-
-% %matrika gridgena
-% %matrika je simetricna in pozitivno definitna
-% load('1311')
-% A = Problem.A;
-% b = A*ones(length(A),1);
-% [X1,FLAG1,RELRES1,ITER1] = gmres(A,b,[],1e-10,100);
-% [X2,FLAG2,RELRES2,ITER2] = minres(A,b,napaka,N);
-%[X3,FLAG3,RELRES3,ITER3] = pcg(A,b,napaka,N,L,U);
-% [X4,FLAG4,RELRES4,ITER4] = bicg(A,b,napaka,N);
-% [X5,FLAG5,RELRES5,ITER5] = qmr(A,b,napaka,N);
-% [X6,FLAG6,RELRES6,ITER6] = symmlq(A,b,napaka,N);
-% [X7,FLAG7,RELRES7,ITER7] = bicgstab(A,b,napaka,N);
-
-
-% %matrika rajat25
-% %matrika ni simetricna in ni pozitivno definitna
-% load('1374')
-% A = Problem.A;
-% b = A*ones(length(A),1);
-% [X1,FLAG1,RELRES1,ITER1] = gmres(A,b,[],napaka,N);
-% [X2,FLAG2,RELRES2,ITER2] = minres(A,b,napaka,N);
-% %[X3,FLAG3,RELRES3,ITER3] = pcg(A,b,napaka,N);
-% %[X4,FLAG4,RELRES4,ITER4] = bicg(A,b,napaka,N);
-% [X5,FLAG5,RELRES5,ITER5] = qmr(A,b,napaka,N);
-% %[X6,FLAG6,RELRES6,ITER6] = symmlq(A,b,napaka,N);
-% %[X7,FLAG7,RELRES7,ITER7] = bicgstab(A,b,napaka,N);
+%matrika 1374
+[casi,ost,koraki,konvergenca]=test1374(napaka,N)
+pause;
+load('1374.mat')
+A = Problem.A;
+b = A*ones(length(A),1);
+[iteracije,ost,cas,konvergenca] = gmresPonovni(A,b,napaka,N,ponovni)
+pause;
